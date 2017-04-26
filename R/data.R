7a6d053aeb6b448c85223aad95632b9416c0247b
merlinoa <merlinoa88@gmail.com>
2017-04-26 16:14:25 -0400

    added RAA data from ChainLadder package

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Data from the Reinsurance Association of America (RAA)
+#'
+#' This data was originally provided by the excellent \code{ChainLadder}
+#' package and was slightly modified for use in \code{devtri}
+#'
+#' @format A data frame with 55 rows and 3 variables:
+#' \describe{
+#'   \item{origin}{the origin period of the losses}
+#'   \item{age}{the development age of the losses}
+#'   \item{value}{the value of the losses}
+#' }
+#'
+"RAA"
