29374767cfe1fd4f7da22957ba80be0d5943f9ad
merlinoa <merlinoa88@gmail.com>
2017-04-28 11:35:29 -0400

    minor docs updates

---
+++ R/tail.R
@@ -0,0 +1,25 @@
+#' tail
+#'
+#' class for ldf tail factor
+#'
+#' @param type type of tail factor
+#' @param args list of arguments to pass to fit that type of tail
+#'
+#'
+#'
+tail <- function(type, args) {
+#  structure(
+#
+#  )
+}
+
+#' tail_linear
+#'
+#' fit a linear tail factor to ldfs
+#'
+#' @param n_points number of ldf points to fit
+#'
+#'
+tail_linear <- function(ldfs, n_points = 2) {
+
+}
