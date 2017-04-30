739237f49e1339f6afac80911813b2f776e6176b
merlinoa <merlinoa88@gmail.com>
2017-04-30 12:02:08 -0400

    interpolating over tail as well as ldfs associated with triangle

--- R/interpolation.R
+++ R/interpolation.R
@@ -13,19 +13,16 @@
 #'
 #' my_cdf <- cdf(ldfs = c(2.70, 1.5, 1.2, 1.15, 1.08, 1.03), first_age = 0.5)
 #'
-#' interpolate_linear(my_cdf, dev = 0.25)
-interpolate_linear <- function(ldfs, dev= 0.5) {
+#' interpolate_linear(my_idf, dev = 0.75)
+interpolate_linear <- function(.cdf, dev= 0.5) {
 
   stopifnot(dev > 0 && dev < 1)
 
   # cannot interpolate 1 ldf
-  stopifnot(nrow(ldfs) > 1)
+  stopifnot(nrow(.cdf) > 1)
 
-  # TODO: remove tail here
-
-
-  hold <- ldfs %>%
-    mutate(
+  hold <- .cdf %>%
+    dplyr::mutate(
       # adjust ldfs to account for ldfs that have not reached the first
       # development age.  These ldfs need to be increased to represent an ldf
       # for the full development period
@@ -42,20 +39,16 @@ interpolate_linear <- function(ldfs, dev= 0.5) {
       earned_ratio = pmin(earned_ratio + dev, 1),
       # rebase cdf based on new earned ratio
       ldf = interp_ldfs * earned_ratio) %>%
-    #filter(!is.na(ldf)) %>%
-    select(age, ldf, earned_ratio)
+    dplyr::filter(!is.na(ldf)) %>%
+    dplyr::select(age, ldf, earned_ratio)
 
-  # TODO: need to update this to handle the tail properly
-  if (is.na(attr(ldfs, "tail_first_age"))) {
-    hold$ldf[hold$age == max(hold$age)] <- 1.0
-    #warning("No tail: setting final development factor to 1.0")
-  }
+  # Can not interpolate to period before any
+  # of the ldfs.  Need extrapolation function if we want to do this.
 
-  # create new cdf object with new tail
+  # create new cdf object
+  # all idf attributes are dropped because triangle and tail stuff are no longer accurate
   cdf(
     ldfs = hold$ldf,
     first_age = min(hold$age, na.rm = TRUE)
-  ) #%>% TDOD: recalculate tail here
-
-
+  )
 }
